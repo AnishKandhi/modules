1e8c58158802b966d23173976c6ee0193408b724
AnishKandhi <110575925+AnishKandhi@users.noreply.github.com>
2023-07-31 19:49:21 -0500

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,9 @@
+import os
+#print(dir(os))
+#print(os.getcwd())
+#print(os.mkdir("/Users/v_kan/Downloads/test"))
+#print(os.path.exists("/Users/v_kan/Downloads/test1"))
+#print(os.listdir())
+root,ext = os.path.splitext("/Users/v_kan/Downloads/search.png")
+print(root)
+print(ext)
---
+++ modules1.py
@@ -0,0 +1,6 @@
+import os
+import shutil
+source = "abc/b.txt"
+destination = "xyz"
+#x = shutil.move(source, destination)
+y = shutil.copy(source, destination)
